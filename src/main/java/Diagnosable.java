bb776e79d609777d048fe6d1666e4d49f1c83130
Håkon Fossum <haakon-rinor@hotmail.com>
2021-02-22 13:35:38 +0100

    Added the Patiet class and the Diagnoasble Interface. Created all the required methods, but the setDiagnoisis method will need some more work later on

---
+++ src/main/java/Diagnosable.java
@@ -0,0 +1,4 @@
+public interface Diagnosable
+{
+    public void setDiagnosis(String diagnosis);
+}
---
+++ src/main/java/Patient.java
@@ -0,0 +1,21 @@
+public class Patient extends Person implements Diagnosable
+{
+    String diagnosis;
+
+    public Patient(String firstName, String lastName, String socialSecurityNumber)
+    {
+        super(firstName, lastName, socialSecurityNumber);
+    }
+
+    public String getDiagnosis()
+    {
+        return diagnosis;
+    }
+
+    @Override
+    public void setDiagnosis(String diagnosis)
+    {
+        this.diagnosis = diagnosis;
+    }
+
+}
