f2ddd459b03066d4e1d591b39b80b0a02ff52451
Håkon Fossum <haakon-rinor@hotmail.com>
2021-03-10 14:26:55 +0100

    Fixed some errors in the pom.xml file and did some minor polish to the main method and the client class itself.

--- src/main/java/no/ntnu/idatg2001/mappeHospital/client/HospitalClient.java
+++ src/main/java/no/ntnu/idatg2001/mappeHospital/client/HospitalClient.java
@@ -1,7 +1,8 @@
 package no.ntnu.idatg2001.mappeHospital.client;
-
 import no.ntnu.idatg2001.mappeHospital.Department;
 import no.ntnu.idatg2001.mappeHospital.Hospital;
+import no.ntnu.idatg2001.mappeHospital.Patient;
+import no.ntnu.idatg2001.mappeHospital.Person;
 import no.ntnu.idatg2001.mappeHospital.exception.RemoveException;
 import no.ntnu.idatg2001.mappeHospital.personel.Employee;
 import no.ntnu.idatg2001.mappeHospital.personel.Nurse;
@@ -15,32 +16,43 @@ public class HospitalClient
    Hospital hospital;
    static HospitalClient client;
 
-    public static void main(String[] args) {
+    /**
+     * This is the main method of the client.
+     * @param args (Not used)
+     */
+    public static void main(String[] args)
+    {
+        // We create the hospital client and run the initialize method.
         client = new HospitalClient();
         client.init();
 
-        //client.numberOfEmployees();
-        //d1.remove(d1.getEmployees().get(0));
-       // client.numberOfEmployees();
-
+        //We create a testEmployee and tries to remove this employee without adding them to the Employee list.
         Employee testEmployee = new Employee("Bjarte", "Olufsen", "442211");
 
-        try
-        {
-            d1.remove(testEmployee);
-        }catch (RemoveException e)
-        {
-           System.out.println(e.getMessage());
-        }
+        client.clientRemove(testEmployee);
 
+        //We let the General practitioner "gp1" set the diagnosis to the patient.
         GeneralPractitioner gp1 = new GeneralPractitioner("Fossum", "Olufsen", "344343");
         gp1.setDiagnosis(d1.getPatients().get(0), "Covid-19");
         System.out.println(d1.getPatients().get(0).getDiagnosis());
 
-        gp1.setDiagnosis(d1.getPatients().get(0), "");
+        //We try to set a diagnosis on a null object and with an empty string.
+        gp1.setDiagnosis(null, "");
         System.out.println(d1.getPatients().get(0).getDiagnosis());
+
+        //We create a new employee and add them to the Empoyees list.
+        Employee p22 = new Employee("Jonas", "Andre", "583483");
+        d1.getEmployees().add(p22);
+        client.numberOfEmployees();
+
+        // We remove the added employee and print out the new number of employees.
+        client.clientRemove(p22);
+        client.numberOfEmployees();
     }
 
+    /**
+     * This method initializes the client. It creates a hospital object, fills the register with data and gets two departments that we  can use.
+     */
     public void init()
     {
         hospital = new Hospital("StOlav");
@@ -49,10 +61,38 @@ public class HospitalClient
         d2 = hospital.getDepartments().get(1); //This is the childrenPolyclinic department.
     }
 
+    /**
+     * This simple method prints out the current number of employees in the list.
+     */
     public void numberOfEmployees()
     {
         System.out.println();
         System.out.println("There are: " + d1.getEmployees().size() + " employees in the Emergency department.");
+    }
+
+    /**
+     * This simple method prints out the current number of employees in the list.
+     */
+    public void numberOfPatients()
+    {
+        System.out.println();
+        System.out.println("There are: " + d1.getPatients().size() + " employees in the Emergency department.");
+    }
+
+    /**
+     * This simple method is created to make the main method more clean. It uses the remove method to remove the person it receives.
+     * @param person the person object to be removed.
+     */
+    public void clientRemove(Person person)
+    {
+        //We use a try catch to handle the situation.
+        try
+        {
+            d1.remove(person);
+        }catch (RemoveException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
